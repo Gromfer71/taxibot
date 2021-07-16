e1b363ff9360f11a0cf3bdd0a8cd35b8121a4fd9
Gromfer71 <usdenis70@gmail.com>
2021-07-16 13:27:33 +0500

    правки

--- app/Services/Address.php
+++ app/Services/Address.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Str;
 class Address
 {
 
-    const MAX_ADDRESS_LENGTH = 255;
+    const MAX_ADDRESS_LENGTH = 240;
     /**
      * @param  string  $query  Адрес который ввел пользователь
      * @param          $cities
