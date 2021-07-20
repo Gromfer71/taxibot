279192fdb23e5990cfc858b33e1fbb805d7079cf
Gromfer71 <usdenis70@gmail.com>
2021-07-20 10:54:14 +0500

    правка по команде

--- app/Console/Commands/SoftCacheClearCommand.php
+++ app/Console/Commands/SoftCacheClearCommand.php
@@ -41,7 +41,7 @@ class SoftCacheClearCommand extends Command
         $this->info('Запустили команду мягкой очистки кеша');
         $maxTime = time()+600;
 
-        while(OrderHistory::whereNull('relevance')->get()->isNotEmpty() && time() <= $maxTime){
+        while(OrderHistory::where('relevance', 0)->get()->isNotEmpty() && time() <= $maxTime){
             sleep(5);
         }
 
