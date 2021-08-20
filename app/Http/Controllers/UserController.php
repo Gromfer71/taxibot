1ff3db6ae8ef11ac5e202d11f73e5e4195b49769
Gromfer71 <usdenis70@gmail.com>
2021-08-20 16:34:31 +0500

    добавлена функция сбросить пользователя в админке

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -107,9 +107,10 @@ class UserController extends Controller
     {
         $botMan = resolve('botman');
         //$bot->userStorage()->save(['vk_id' => User::where('id', $id)->first()->vk_id]);
-        $botMan->say('тест', '1585139223', TelegramDriver::class);
-        $botMan->removeStoredConversation();
+        //$botMan->say('тест', '1585139223', TelegramDriver::class);
+
         $botMan->startConversation(new StartConversation(), User::where('id', $id)->first()->telegram_id, TelegramDriver::class);
+        $botMan->startConversation(new StartConversation(), User::where('id', $id)->first()->vk_id, VkCommunityCallbackDriver::class);
 
         return back();
 
