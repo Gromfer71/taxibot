9af6051299ffd1fc173911ca1ed6881e6baedfcc
Gromfer71 <usdenis70@gmail.com>
2021-09-10 09:29:52 +0500

    добавлен журнал ошибок

--- database/migrations/2021_09_10_042725_change_user_id_in_error_reports_to_bigint.php
+++ database/migrations/2021_09_10_042725_change_user_id_in_error_reports_to_bigint.php
@@ -14,7 +14,7 @@ class ChangeUserIdInErrorReportsToBigint extends Migration
     public function up()
     {
         Schema::table('error_reports', function (Blueprint $table) {
-            $table->bigIncrements('user_id')->change();
+            $table->bigInteger('user_id')->change();
         });
     }
 
