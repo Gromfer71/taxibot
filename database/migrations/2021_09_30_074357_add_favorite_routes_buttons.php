cdeee7607dd5b960ac2ec3075469fa08e6de3b1b
Gromfer71 <usdenis70@gmail.com>
2021-09-30 12:46:00 +0500

    Шаблонные маршруты

---
+++ database/migrations/2021_09_30_074357_add_favorite_routes_buttons.php
@@ -0,0 +1,58 @@
+<?php
+
+use Barryvdh\TranslationManager\Models\Translation;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFavoriteRoutesButtons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Translation::firstOrCreate(
+            [
+                'locale' => 'ru',
+                'group' => 'buttons',
+                'key' => 'delete route',
+                'value' => 'Удалить маршрут',
+            ]
+        );
+        Translation::firstOrCreate(
+            [
+                'locale' => 'ru',
+                'group' => 'messages',
+                'key' => 'delete route menu',
+                'value' => 'Меню удаления маршрутов',
+            ]
+        );
+        Translation::firstOrCreate(
+            [
+                'locale' => 'ru',
+                'group' => 'messages',
+                'key' => 'confirm delete favorite route',
+                'value' => 'Подтвердите удаление маршрута',
+            ]
+        );
+        Translation::firstOrCreate(
+            [
+                'locale' => 'ru',
+                'group' => 'messages',
+                'key' => 'write favorite route name',
+                'value' => 'Напишите псевдоним маршрута',
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
