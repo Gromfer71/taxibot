a05c6c42daf184e56e643cf65d8a5b9d513d6300
Gromfer71 <usdenis70@gmail.com>
2021-10-01 10:06:04 +0500

    Добавление маршрута после завершение заказа

---
+++ database/migrations/2021_10_01_050506_add_favorite_route_buttons.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddFavoriteRouteButtons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Translation::firstOrCreate(
+            [
+                'locale' => 'ru',
+                'group' => 'messages',
+                'key' => 'added favorite route menu',
+                'value' => 'Итак, ваш маршрут ',
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
