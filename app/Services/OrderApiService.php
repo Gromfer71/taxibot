e7c8138430dc4bd4b5f674fea8eb73143fc7264a
Gromfer <usdenis70@gmail.com>
2021-12-07 10:47:35 +0500

    Правки

--- app/Services/OrderApiService.php
+++ app/Services/OrderApiService.php
@@ -12,9 +12,7 @@ use BotMan\BotMan\BotMan;
 use BotMan\BotMan\Storages\Storage;
 use BotMan\Drivers\Telegram\TelegramDriver;
 use BotMan\Drivers\VK\VkCommunityCallbackDriver;
-use Exception;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Log;
 
 class OrderApiService
 {
@@ -439,24 +437,32 @@ class OrderApiService
 
     private function file_get_contents_with_logging($url, $params)
     {
-        $success = false;
-        $counter = 0;
-        do {
-            try {
-                $context = stream_context_create(['http' => $params]);
-                $log = LogApi::newLogApi($url, json_encode($params, JSON_UNESCAPED_UNICODE));
-                $result = file_get_contents($url, false, $context);
-                $log->result = $result;
-                $log->save();
-                $success = true;
-            } catch (Exception $exception) {
-                $counter++;
-                Log::error($exception->getMessage());
-                sleep(1);
-                $success = false;
-            }
-        } while (!$success || $counter < 1000);
+        $context = stream_context_create(['http' => $params]);
+        $log = LogApi::newLogApi($url, json_encode($params, JSON_UNESCAPED_UNICODE));
+        $result = file_get_contents($url, false, $context);
+        $log->result = $result;
+        $log->save();
+        return $result;
+//        $success = false;
+//        $counter = 0;
+//        do {
+//            try {
+//                $context = stream_context_create(['http' => $params]);
+//                $log = LogApi::newLogApi($url, json_encode($params, JSON_UNESCAPED_UNICODE));
+//                $result = file_get_contents($url, false, $context);
+//                $log->result = $result;
+//                $log->save();
+//                $success = true;
+//            } catch (Exception $exception) {
+//                $counter++;
+//                Log::error($exception->getMessage());
+//                sleep(1);
+//                $success = false;
+//            }
+//        } while (!$success || $counter < 1000);
+//
+//        return $result ?? null;
 
-        return $result ?? null;
+
     }
 }
