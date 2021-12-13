fe651f2006be2f0aa0913003265cd3a70f8d52fc
Gromfer <usdenis70@gmail.com>
2021-12-13 10:39:53 +0500

    Тест

--- app/Console/Commands/CheckOrderStateCommand.php
+++ app/Console/Commands/CheckOrderStateCommand.php
@@ -52,14 +52,14 @@ class CheckOrderStateCommand extends Command
         $this->info('Запустили команду проверки статусов заказов');
 
         $finishTime = time() + 57;
-        $targetTimeToEveryExecute = 3000000;//В микросекундах
+        $targetTimeToEveryExecute = 1000000;//В микросекундах
 
         $botMan = resolve('botman');
 
         while (time() <= $finishTime) {
-            $timestart = microtime(true) * 3000000;
+            $timestart = microtime(true) * 1000000;
             $this->_handle_once($botMan);
-            $timeEnd = microtime(true) * 3000000;
+            $timeEnd = microtime(true) * 1000000;
             $timeToSlep = $targetTimeToEveryExecute - ($timeEnd - $timestart);
             if ($timeToSlep > 0) {
                 usleep($timeToSlep);
@@ -107,7 +107,6 @@ class CheckOrderStateCommand extends Command
             }
 
             // если статус заказа поменялся, только тогда производим какие-то действия
-            $botMan->say('Допустим, диспетчер изменил ваш адрес.', $recipientId, $driverName);
             if (!$newStateId) {
                 continue;
             }
