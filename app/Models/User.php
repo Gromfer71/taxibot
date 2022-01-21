0f35ac7281c983e792649178f96f6a00226061a7
Gromfer71 <usdenis70@gmail.com>
2022-01-21 11:54:20 +0500

    Правки

--- app/Models/User.php
+++ app/Models/User.php
@@ -113,7 +113,7 @@ class User extends Model
      */
     public function addresses()
     {
-        return $this->hasMany(AddressHistory::class, 'user_id', 'id')->orderBy('updated_at', 'desc')->take(25);
+        return $this->hasMany(AddressHistory::class, 'user_id', 'id')->orderBy('updated_at', 'desc')->take(24);
     }
 
     /**
@@ -123,7 +123,7 @@ class User extends Model
      */
     public function favoriteAddresses()
     {
-        return $this->hasMany(FavoriteAddress::class, 'user_id', 'id')->orderBy('created_at', 'desc')->take(25);
+        return $this->hasMany(FavoriteAddress::class, 'user_id', 'id')->orderBy('created_at', 'desc')->take(24);
     }
 
     /**
