a3487ed06ee3624cd4f6d1861bca9566e826dbcc
Gromfer71 <usdenis70@gmail.com>
2022-03-14 18:30:49 +0500

    фикс

---
+++ database/migrations/2022_03_14_182940_make_file_name_nullable.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeFileNameNullable extends Migration
+{
+    public function up()
+    {
+        Schema::create('global_messages', function (Blueprint $table) {
+            $table->string('file_name')->nullable()->after('file')->change();
+        });
+    }
+
+    public function down()
+    {
+    }
+}
