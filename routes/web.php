c6b9b2309f8800ef6d8dcd1574055e7d07e92b8e
Gromfer71 <usdenis70@gmail.com>
2022-03-14 13:17:13 +0500

    фикс

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,6 @@ Route::group(['midlleware' => 'auth'], function () {
     Route::get('/messages/index', 'MessagesController@index')->name('messages.index');
     Route::post('/messages/send', 'MessagesController@send')->name('messages.send');
     Route::get('/messages/delete/{id}', 'MessagesController@deleteMessage')->name('messages.delete-message');
-    Route::post('/messages/clearAllMessages', 'MessagesController@clearAllMessages')->name('messages.clear-all-messages');
+    Route::get('/messages/clearAllMessages', 'MessagesController@clearAllMessages')->name('messages.clear-all-messages');
 });
 
