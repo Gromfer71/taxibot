861bdd6e852a69be0acbf184a2a791cdbbd9b598
Gromfer71 <usdenis70@gmail.com>
2022-03-23 17:28:59 +0500

    режим тех обслуживания

--- app/Botman.php
+++ app/Botman.php
@@ -6,10 +6,10 @@ class Botman extends \BotMan\BotMan\BotMan
 {
     public function loadActiveConversation()
     {
-//        if (env('MAINTENANCE_MODE')) {
-//            $this->reply('В чат-боте ведутся технические работы. Приносим извинения за неудобства');
-//            die();
-//        }
+        if (env('MAINTENANCE_MODE')) {
+            $this->reply('В чат-боте ведутся технические работы. Приносим извинения за неудобства');
+            die();
+        }
 
         parent::loadActiveConversation();
     }
